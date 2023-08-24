becafadcb73b67265e0afb5bf9402954589fe249
Andrew Miroshnichenko <mobilemiron@gmail.com>
2023-08-24 12:36:05 +0300

    refactor: removed redundant dependencies from app module

--- app/src/main/java/com/example/tracker/TrackerApp.kt
+++ app/src/main/java/com/example/tracker/TrackerApp.kt
@@ -3,17 +3,11 @@ package com.example.tracker
 import android.app.Application
 import androidx.hilt.work.HiltWorkerFactory
 import androidx.work.Configuration
-import com.google.firebase.FirebaseApp
 import dagger.hilt.android.HiltAndroidApp
 import javax.inject.Inject
 
 @HiltAndroidApp
 class TrackerApp : Application(), Configuration.Provider {
-
-    override fun onCreate() {
-        super.onCreate()
-        FirebaseApp.initializeApp(this)
-    }
 
     @Inject
     lateinit var workerFactory: HiltWorkerFactory
